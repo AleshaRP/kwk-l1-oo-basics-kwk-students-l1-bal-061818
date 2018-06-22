0a955791601b6c1c7a87285db1f53a17cbf78be1
AleshaRP <createlele@yahoo.com>
2018-06-22 15:21:58 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -29,6 +29,7 @@ class Book
    def turn_page
      puts "Flipping the page... wow, you read fast"
    end
+ end
 
    Harry_Potter = Book.new ("Harry Potter and the Sorcerer's Stone")
    Harry_Potter.turn_page
